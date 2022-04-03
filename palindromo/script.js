f18cad8958214b0557f5b23b051ea5e2e2d89bd0
Chicco80 <chicco80lisa@gmail.com>
2022-04-03 11:26:27 +0200

    palindromo ok

---
+++ palindromo/script.js
@@ -0,0 +1,32 @@
+//chiedere ad utente di scrivere una parola palindroma
+
+
+const parolaUtente = prompt("scrivi un parola");
+// console.log(parolaUtente);
+
+let checkParola;
+
+//trasformiamo la parola inserita in un array
+let arrayParUtente = parolaUtente.split("");
+console.log(arrayParUtente)
+
+
+//invertiamo gli array
+
+arrayParUtente = arrayParUtente.reverse("");
+console.log(arrayParUtente)
+
+
+//trasformo array in stringa al contrario
+
+ let parolaRev = arrayParUtente.join("");
+ console.log(parolaRev);
+
+
+ //confronto che parola inserita e parola inserita al contrario siano uguali
+
+ if(parolaUtente === parolaRev){
+     alert("la tua parola è palindroma")
+ }else{
+     alert("la tua parola non è palindroma")
+ }
